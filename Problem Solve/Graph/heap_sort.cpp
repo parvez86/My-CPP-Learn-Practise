e7e06d430e12f321cc5be69a127301ffe3d671ca
Shahriar Parvez <shahriarp86@gmail.com>
2023-04-04 09:11:32 +0600

    update heap_sort.cpp

--- Problem Solve/Graph/heap_sort.cpp
+++ Problem Solve/Graph/heap_sort.cpp
@@ -1,112 +1,122 @@
+// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
-
 using namespace std;
-void print_vec(vector<int> vec);
 
-void min_heapify(vector<int>& vec, int i, int n)
-{
-
-    int largest = i;
-    int l = 2*i+1;
-    int r = 2*i+2;
-
-    // if left child is larger than root
-    if(l<n && vec[l]>vec[largest]){
-        largest = l;
+    class Solution{
+    void swap(int *a, int *b){
+        int temp=*a;
+        *a = *b;
+        *b = temp;
     }
-
-    // if right child is larger than root
-    if(r<n && vec[r]>vec[largest]){
-        largest = r;
+    void maxHeapify(vector<int>& nums, int indx, int n){
+        // root-> indx
+        int largest = indx;
+        int l = 2*indx+1;
+        int r = 2*indx+2;
+        
+        if(l<n && nums[l]>nums[largest]){
+            largest = l;
+        }
+        if(r<n && nums[r]>nums[largest]){
+            largest = r;
+        }
+        
+        if(largest != indx){
+            // swap(&nums[indx] && nums[largest]);
+            // swap
+            int temp=nums[largest];
+            nums[largest]=nums[indx];
+            nums[indx]=temp;
+            
+            // goes to the root
+            maxHeapify(nums, largest, n);
+        }
     }
-
-    // if largest is not root
-    // swap the root & child
-    // recursively
-    if(largest != i){
-        swap(vec[i], vec[largest]);
-
-        // min_heapify the affected sub-tree
-        min_heapify(vec, largest, i);
+    void minHeapify(vector<int>& nums, int indx, int n){
+        // root-> indx
+        int smallest = indx;
+        int l = 2*indx+1;
+        int r = 2*indx+2;
+        
+        if(l<n && nums[l]<nums[smallest]){
+            smallest = l;
+        }
+        if(r<n && nums[r]<nums[smallest]){
+            smallest = r;
+        }
+        
+        if(smallest != indx){
+            // swap(&nums[indx] && nums[largest]);
+            // swap
+            int temp=nums[smallest];
+            nums[smallest]=nums[indx];
+            nums[indx]=temp;
+            
+            // goes to the root
+            minHeapify(nums, smallest, n);
+        }
     }
-}
-
-void heap_sort_min(vector<int> vec)
-{
-    // build heap
-    for(int i = vec.size()/2; i>=0; i--){
-        min_heapify(vec, i, vec.size()-1); // min_heapify(arr, item, root)
+public:
+    // time complexity: all-> O(nlogn), space: 0(1)
+    // ascending order
+    void buildMaxHeap(vector<int> nums){
+        
+        // 1st build heap & heapify: log2(N)[0, N/2]
+        for(int i=nums.size()/2-1; i>=0; i--){
+            maxHeapify(nums, i, nums.size()-1);
+        }
+        
+        // heapify: [0, N-1]
+        for(int i=nums.size()-1; i>=0;i--){
+            // swap(&nums[0], &nums[i]);
+            
+            // store max root value
+            // swap
+            int temp=nums[0];
+            nums[0]=nums[i];
+            nums[i]=temp;
+            
+            maxHeapify(nums, 0, i);
+        }
+        cout<< "After heap(max) sort the items are: ";
+        for(int i=0; i<nums.size(); i++){
+            cout<< nums[i]<< " ";
+        }
+        cout<< endl;
     }
-
-    // extract one by one
-    for(int i=vec.size()-1; i>0; i--){
-        swap(vec[0], vec[i]);
-
-        // call min_heapify
-        min_heapify(vec, 0, i);
+    
+    // descending order
+    void buildMinHeap(vector<int> nums){
+        
+        // 1st build heap & heapify: log2(N)[0, N/2]
+        for(int i=nums.size()/2-1; i>=0; i--){
+            minHeapify(nums, i, nums.size()-1);
+        }
+        
+        // heapify: [0, N-1]
+        for(int i=nums.size()-1; i>=0;i--){
+            // swap(&nums[0], &nums[i]);
+            
+            // store min root value
+            // swap
+            int temp=nums[0];
+            nums[0]=nums[i];
+            nums[i]=temp;
+            
+            minHeapify(nums, 0, i);
+        }
+        cout<< "After heap(min) sort the items are: ";
+        for(int i=0; i<nums.size(); i++){
+            cout<< nums[i]<< " ";
+        }
+        cout<< endl;
     }
-    print_vec(vec);
-}
+};
 
-void max_heapify(vector<int>& vec, int i, int n)
-{
-    int largest = i;
-    int l = 2*i+1;
-    int r = 2*i+2;
-
-
-    // if left child is smaller than root
-    if(l<n && vec[l] < vec[largest]){
-        largest = l;
-    }
-
-    // if right child is smaller than root
-    if(r<n && vec[r] < vec[largest]){
-        largest = r;
-    }
-
-    // if largest is not root
-    // swap the root & child
-    // recursively
-    if(largest != i){
-        swap(vec[i], vec[largest]);
-
-        // max_heapify the affected sub-tree
-        max_heapify(vec, largest, i);
-    }
-}
-
-void heap_sort_max(vector<int> vec)
-{
-    // build heap
-    for(int i = vec.size()/2; i>=0; i--){
-        max_heapify(vec, i, vec.size()-1); // max_heapify(arr, item, root)
-    }
-
-    // extract one by one
-    for(int i=vec.size()-1; i>0; i--){
-        swap(vec[0], vec[i]);
-
-        // call max_heapify
-        max_heapify(vec, 0, i);
-    }
-    print_vec(vec);
-}
-
-void print_vec(vector<int> vec)
-{
-    cout<< "The items of tree: ";
-    for(const auto it:vec) cout<< it<< " ";
-    cout<< endl;
-}
-
-// time complexity=O(nlogn) (all cases), space complexity= O(1)
-int main()
-{
-    vector<int> vec {12, 11, 13, 5, 6, 7};
-    // ascending order -> min-heap
-    heap_sort_min(vec);
-    // descending order -> max-heap
-    heap_sort_max(vec);
+int main() {
+    Solution obj;
+    vector<int> nums = {1, 2, 7, 9, 3, 6, 5, 4, 8};
+    obj.buildMaxHeap(nums);
+    obj.buildMinHeap(nums);
     return 0;
 }
