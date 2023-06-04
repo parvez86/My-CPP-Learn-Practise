61d6abca64315d196f7fe558e2e1b2f79074de62
Shahriar Parvez <shahriarp86@gmail.com>
2023-06-04 18:33:40 +0600

    Update LeetCode_236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp

--- Problem Solve/Tree/BT/LeetCode_236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ Problem Solve/Tree/BT/LeetCode_236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    TreeNode* travaresePostOrder(TreeNode* root, TreeNode* p, TreeNode* q){
+    TreeNode* traverse(TreeNode* root, TreeNode* p, TreeNode* q){
         if(!root) return NULL;
         if (root->val==p->val || root->val == q->val) return root;
-        TreeNode* l_child = travaresePostOrder(root->left, p, q);
+        TreeNode* l_child = traverse(root->left, p, q);
         if(l_child && ((l_child->val == p->val && root->val==q->val)||  (l_child->val == q->val && root->val==p->val))) return root;
-        TreeNode* r_child = travaresePostOrder(root->right, p, q);
+        TreeNode* r_child = traverse(root->right, p, q);
         if(r_child && ((r_child->val == p->val && root->val==q->val)||  (r_child->val == q->val && root->val==p->val))) return root;
 
         return (l_child && r_child)? root: (l_child)?l_child:r_child;
     }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return travaresePostOrder(root, p, q);
+        return traverse(root, p, q);
     }
 };
